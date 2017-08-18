6d08c3c1d653e6a16f0248c5b80f470be25b074b
Mohammad Sazzad Hossan <sazzad2324@gmail.com>
2017-08-18 13:52:49 +0200

    Component decoration 1

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a Test', 'http://maxpixel.freegreatpicture.com/static/photo/640/Recipe-Salty-Dog-Party-Drink-Alcohol-Cocktail-831762.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
